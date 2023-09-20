53b168342547012616b35ea322fa106913039df3
Jeremy <jdavid.rupprecht@gmail.com>
2023-09-20 16:54:27 -0600

    Implement clicking and draging to draw on the grid

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
 function createDivs (numberOfDivs) {
-    divs = [];
     for (let i = 0; i < numberOfDivs; i++) {
         const div = document.createElement("div");
         div.classList.add("gridElement");
+
+        // To disable the browers default dragging behavior
+        div.ondragstart = () => {return false};
+
         grid.appendChild(div);
-
-        divs.push(div);
+        div.addEventListener("mousemove", changeColor);
     }
-
-    return divs
 }
 
+function changeColor(e) {
+    if (!mouseDown) return;
+    this.style.backgroundColor = "black";
+
+}
+
+// Track mousemove events only when when the mouse is down, instead of tracking
+// mousedown events which have issues with dragging 
+
+let mouseDown = false;
+document.addEventListener("mousedown", () => mouseDown = true);
+document.addEventListener("mouseup", () => mouseDown = false);
+
 const grid = document.getElementById("grid");
-const divList = createDivs(256);
-console.log(grid);
+createDivs(256);
+
