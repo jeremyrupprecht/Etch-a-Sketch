4cd98e27e42b4a0bb5a032f3cfdaa707dd1d9f47
Jeremy <jdavid.rupprecht@gmail.com>
2023-09-20 14:20:10 -0600

    Create a static 16x16 grid of divs

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+function createDivs (numberOfDivs) {
+    divs = [];
+    for (let i = 0; i < numberOfDivs; i++) {
+        const div = document.createElement("div");
+        div.classList.add("gridElement");
+        grid.appendChild(div);
 
+        divs.push(div);
+    }
+
+    return divs
+}
+
+const grid = document.getElementById("grid");
+const divList = createDivs(256);
+console.log(grid);
